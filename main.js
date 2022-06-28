2111eb102418b5bb0dfdaab23d42d6283cf88503
devhongsa <bippr0901@gmail.com>
2022-06-29 03:46:37 +0900

    u

--- main.js
+++ main.js
@@ -1,41 +1,17 @@
-function solution(N, arr) {
-    let res = 0;
+let result = 100;
 
-    for (i=0; i<arr.length-1; i++){
-        for (j=i+1; j<arr.length; j++){
-            if (arr[i] + arr[j] == 0) return 2
-        }
-    }
+let num = 4;
 
-    for (i=0; i<arr.length-2; i++){
-        for (j=i+1; j<arr.length-1; j++){
-            for (m=j+1; m<arr.length; m++){
-                if (arr[i] + arr[j] + arr[m] == 0) return 3
-            }
-        }
-    }
+let count = 0;
 
-    for (i=0; i<arr.length-3; i++){
-        for (j=i+1; j<arr.length-2; j++){
-            for (m=j+1; m<arr.length-1; m++){
-                for (n=m+1; n<arr.length; n++){
-                    if (arr[i] + arr[j] + arr[m] + arr[n] == 0) return 4
-                }
-            }
-        }
-    }
 
-    for (i=0; i<arr.length-4; i++){
-        for (j=i+1; j<arr.length-3; j++){
-            for (m=j+1; m<arr.length-2; m++){
-                for (n=m+1; n<arr.length-1; n++){
-                    for (b=n+1; b<arr.length; b++){
-                        if (arr[i] + arr[j] + arr[m] + arr[n] + arr[b] == 0) return 5
-                    }
-                }
-            }
-        }
-    }
+while ( result > 8 ) {
 
-    return -1;
+    result = result - num;
+
+    count = count + 1;
+
 }
+
+
+console.log(count);
