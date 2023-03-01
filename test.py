327042b7c13b69b78173681bb12dbce4a0d027b3
devhongsa <bippr0901@gmail.com>
2023-03-01 17:24:25 +0900

    coding test study

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-string = "ohelloworld"
+def DFS(v):
+    global cnt
+    if v == m :
+        print(res)
+        cnt+=1
+        return
+    else:
+        for i in range(1,n+1):
+            if checkList[i]==0:
+                res[v] = i
+                checkList[i]=1
+                DFS(v+1)
+                checkList[i]=0
+                print("checkList reset")
 
-print(string.rfind("o",0,6))
+if __name__ == "__main__":
+    n = 4
+    m = 3
+    cnt = 0
+    checkList = [0]*(n+1)
+    res = [0]*m
+    
+    DFS(0)
+    print(cnt)
